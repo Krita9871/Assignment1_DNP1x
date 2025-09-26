cc4b9d1112ebe6abc8409ef585f8d41c52499cb9
Krita9871 <vyaliyvarenick2.z@gmail.com>
2025-09-26 16:09:22 +0200

    FileRepository namespace

--- Server/Entities/obj/Debug/net10.0/Entities.AssemblyInfo.cs
+++ Server/Entities/obj/Debug/net10.0/Entities.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Entities")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+78ecd0bc37069cf5c9eb858821e56b7b9f1915c5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b561786ed7d750c3da5cd9fb189227fe624cfa5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Entities")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Entities")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
